b2f4b5f4a23766d77729a251d4451877e4dd8713
vladtanasiedns <tanasie.vlad@dandsltd.com>
2021-04-15 10:28:42 +0300

    Created skeleton class inside DellModelPrediction.py, need to solve issues as errors appear when new values are added to columns as the encoder does not know them

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from google.cloud import bigquery
+from DellModelPrediction import DellModelPrediction
+# from google.cloud import bigquery
 
-BigQuery_client = bigquery.client.Client()
+# BigQuery_client = bigquery.client.Client()
+
+network = DellModelPrediction(model_path="model_recall_0.7462920956203106", encoder_path="missing_data_encoder (1).joblib", data_path="dell_data.csv")
+
 
 
 
