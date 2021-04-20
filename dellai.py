eb1dab5e6aef30a07caf869fccb8b2d94ec71269
vladtanasiedns <tanasie.vlad@dandsltd.com>
2021-04-20 18:11:08 +0300

    Modified encoding function, added error handling also added instructions for building predictSequentially function

--- dellai.py
+++ dellai.py
@@ -2,10 +2,15 @@ from tensorflow import keras
 from joblib import load
 import pandas as pd
 import numpy as np
+import os
+
 
 # TODO: Handle unseen values that get into the model (OOV - Out of Vocabulary values)
 # TODO: Refactor docstrings for the class, add better explanations
 # TODO: Add better error handling for the functions
+# TODO: Refactor the name of the library and functions
+# TODO: Output a requirements.txt file
+# TODO: Separate DataFrame from encoded data
 
 
 class SlowDispatchPrediction:
@@ -24,8 +29,11 @@ class SlowDispatchPrediction:
             To make a prediction use the `predict` function
 
             Usage:
-                Initiate the object, pass the path values of your data, encoder and model then use the predict model to get the prediction.
-                If the paths are not available at the time of instantiation, call the, loadModel, loadData and loadEncoder functions by passing the required objects,
+                Initiate the object, pass the path values of your data, encoder and model and then use the predictBatch
+                and predictSequentially functions to get your predictions.
+                Currently supported file types are csv, json, xlsx, xls, odf, ods, odt
+                If the paths are not available at the time of instantiation, call the, loadModel, loadData and
+                loadEncoder functions by passing the required objects,
                 then call the sanitize, encode and predict functions.
         """
         if model_path is not None:
@@ -35,9 +43,18 @@ class SlowDispatchPrediction:
             self.encoder = load(encoder_path)
 
         if data_path is not None:
-            # TODO: Check for file types and create apropriate data frame from those data types, goals (xls, xlsx, json, obs)
+            name, extension = os.path.splitext(data_path)
 
-            self.data = pd.DataFrame(pd.read_csv(data_path))
+            if extension is ".csv":
+                self.data = pd.read_csv(data_path)
+            elif extension is ".json":
+                self.data = pd.read_json(data_path)
+            elif extension is ".xlsx":
+                self.data = pd.read_excel(data_path)
+            elif extension is ".xls":
+                self.data = pd.read_excel(data_path, engine="xlrd")
+            elif extension is ".odf" or ".ods" or ".odt":
+                self.data = pd.read_excel(data_path, engine="odf")
 
         # The categorical columns that the model needs
         self.categorical_columns = ['COUNTRY', 'SOURCE', 'DISPATCH_TYPE', 'CALL_TYPE',
@@ -58,6 +75,9 @@ class SlowDispatchPrediction:
                                   'TMZN_LOC_ID', 'DW_LD_GRP_VAL', 'ITM_COST', 'ISS_CODE', 'COST', 'TWO_WEEK_TR',
                                   'SPMD_FBSC', 'OneCost']
 
+        # Private variable of the encoded version of the data
+        self._encoded_data = None
+
         # If there is a path for the encoder and for the data the class will sanitize and encode the data for the model
         if encoder_path is not None and data_path is not None:
             self.sanitize()
@@ -65,7 +85,7 @@ class SlowDispatchPrediction:
 
     def loadModel(self, model):
         """
-        model - already loaded from tf.keras.model.load
+        model - already loaded from tf.keras.model.load('model path')
         """
         self.model = model
 
@@ -96,10 +116,10 @@ class SlowDispatchPrediction:
             numerical = [col for col in numerical.columns]
             categorical = [col for col in categorical.columns]
 
-            # Loop trough categorical columns and replace missing valaues
+            # Loop trough categorical columns and replace missing values
             for col in categorical:
                 self.data[col].fillna(value="NULL", inplace=True)
-            # Loop trough numerical columns and replace missing valaues
+            # Loop trough numerical columns and replace missing values
             for col in numerical:
                 self.data[col].fillna(value=np.float(0), inplace=True)
 
@@ -110,22 +130,58 @@ class SlowDispatchPrediction:
 
     def encode(self):
         """
-        Encodes the data property of the object to numerical for making a prediction
+        Encodes the data property of the object to numerical for making a prediction.
+        WARNING! If the data that is predicted on contains any extra data that the model has not seen,
+        it will throws an error
         """
-        if self.data is not None:
-            self.data = self.encoder.transform(self.data)
+        try:
+            if self.data is not None:
+                self._encoded_data = self.encoder.transform(self.data)
+        except Exception:
+            print("There was an error while encoding the data")
+            print(Exception)
 
     def predictBatch(self, return_dict=False):
+        # TODO: Implement saving the predictions to file
+        """
+        Use this function if there were no errors during the encoding of the data
+
+        :param return_dict: if True returns a dictionary with the predicted classes and the probabilities
+                for the classes
+        :return: array of predicted classes
+        """
         if self.data is not None:
             if not return_dict:
-                return (self.model.predict(self.data) > 0.5).astype("int32")
+                return (self.model.predict(self._encoded_data) > 0.5).astype("int32")
             else:
                 return {
-                    "classes": (self.model.predict(self.data) > 0.5).astype("int32"),
-                    "probability": self.model.predict(self.data)
+                    "classes": (self.model.predict(self._encoded_data) > 0.5).astype("int32"),
+                    "probability": self.model.predict(self._encoded_data)
                 }
         else:
+            print("Data has not been passed to the object, please use loadData(data_path)")
             return None
+
+    def predictSequentially(self, output_csv=False):
+        """
+            Predicts if a data sample will be a slow dispatch, it does this sequentially by looping through the data,
+        this might be inefficient computationally but it has error handling for those cases where the encoder encounters
+        data that it has not been trained to encode
+
+        :param output_csv: if True will save a csv file with the predictions as a new column in the original data
+        :return: dictionary of predicted classes, probabilities and data samples that had errors
+        """
+
+        # TODO: Implement predicting sequentially on the data and return predictions,
+        #  for error sample save them separately
+        # TODO: Implement saving the results to file and save in the same file predictions and error samples
+
+        # Read file
+        #   Loop trough data samples then encode and predict each row
+        #       Handle possible errors and save the error rows for later
+        #   Match prediction with the row in the data and save the row and the prediction in a dict
+        #   Save predictions and errors in a new column on the data passed in a csv file
+        pass
 
     def __str__(self):
         return str(self.model.summary())
