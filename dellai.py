d1c9c9b71c3e7f459e9619b66d881efd9f2e32a1
vladtanasiedns <tanasie.vlad@dandsltd.com>
2021-04-21 16:54:13 +0300

    Implemented predictSequentially function, it sends a dict with the results or saves a csv file with the initial data and the results as new columns, created new todos

--- dellai.py
+++ dellai.py
@@ -5,13 +5,11 @@ import numpy as np
 import os
 
 
-# TODO: Handle unseen values that get into the model (OOV - Out of Vocabulary values)
 # TODO: Refactor docstrings for the class, add better explanations
 # TODO: Add better error handling for the functions
-# TODO: Refactor the name of the library and functions
 # TODO: Output a requirements.txt file
-# TODO: Separate DataFrame from encoded data
-
+# TODO: Plan and implement a way to update/train the model remotely
+# TODO: Implement loading data to a dataframe from sql
 
 class SlowDispatchPrediction:
     def __init__(self, model_path=None, encoder_path=None, data_path=None):
@@ -37,63 +35,60 @@ class SlowDispatchPrediction:
                 then call the sanitize, encode and predict functions.
         """
         if model_path is not None:
-            self.model = keras.models.load_model(model_path)
+            self._model = keras.models.load_model(model_path)
 
         if encoder_path is not None:
-            self.encoder = load(encoder_path)
+            self._encoder = load(encoder_path)
 
         if data_path is not None:
             name, extension = os.path.splitext(data_path)
-
-            if extension is ".csv":
+            if extension == ".csv":
                 self.data = pd.read_csv(data_path)
-            elif extension is ".json":
+            elif extension == ".json":
                 self.data = pd.read_json(data_path)
-            elif extension is ".xlsx":
+            elif extension == ".xlsx":
                 self.data = pd.read_excel(data_path)
-            elif extension is ".xls":
+            elif extension == ".xls":
                 self.data = pd.read_excel(data_path, engine="xlrd")
-            elif extension is ".odf" or ".ods" or ".odt":
+            elif extension == ".odf" or ".ods" or ".odt":
                 self.data = pd.read_excel(data_path, engine="odf")
 
         # The categorical columns that the model needs
-        self.categorical_columns = ['COUNTRY', 'SOURCE', 'DISPATCH_TYPE', 'CALL_TYPE',
-                                    'REASON_CODE', 'CREATOR_LOGIN', 'OWNER_LOGIN', 'STATUS', 'PARTS_STATUS',
-                                    'LOCAL_CHANNEL', 'SVC_TYPE', 'SVC_LEVEL', 'SVC_OPTIONS', 'PROD_TYPE_DESC',
-                                    'PROD_GRP_DESC', 'LOB_CODE', 'LOB_DESC', 'PROD_LN_DESC', 'SYS_CLASS',
-                                    'ZIP_SHIPPING', 'KYHD', 'ACCIDENTAL_DAMAGE', 'COMPLETE_CARE', 'TAX_XMPTN_FLG',
-                                    'CURRENCY_CODE', 'KYC', 'ITM_NBR', 'DSPCH_ORD_STAT_CD', 'MOST_XPSV_PART_FLG',
-                                    'DSTRB_HUB_NM', 'PART_TYPE_CD', 'ITEM_NUM', 'ITEM_DESC', 'AGE_CLASS', 'COMMODITY',
-                                    'COMMODITY_DESC', 'ASP_FAMILY', 'RFC', 'REPAIR', 'RSL_ACTIVE_FLAG',
-                                    'BOM_Parent_Part', 'AMER_PLNR_NAME', 'EMEA_PLNR_NAME', 'APJ_PLNR_NAME',
-                                    'FULFIL_CENTER', 'SPMD_ACTIVE_FLAG', 'SPMD_COMMODITY']
+        self.categorical_columns = ['HEADER_SVC_BU_ID', 'HEADER_DSPCH_ORD_BU_ID', 'TECH_DRCT_FLG', 'B2C_FLG', 'B2D_FLG',
+                                    'DUMMY_SVC_TAG_FLG', 'NEW_PART_SHIP_FLG', 'SHIP_SVC_BU_ID', 'ITM_QTY',
+                                    'ITM_COMDTY_ID', 'PART_ORD_SEQ_NBR', 'PART_ORD_LN_NBR', 'NEW_PART_FLG',
+                                    'SUBSTT_FLG', 'TMZN_LOC_ID', 'DW_LD_GRP_VAL', 'ITM_COST', 'ISS_CODE', 'COST',
+                                    'TWO_WEEK_TR', 'SPMD_FBSC', 'OneCost']
 
         # The numerical columns that the model needs
-        self.numerical_columns = ['HEADER_SVC_BU_ID', 'HEADER_DSPCH_ORD_BU_ID', 'TECH_DRCT_FLG', 'B2C_FLG', 'B2D_FLG',
-                                  'DUMMY_SVC_TAG_FLG', 'NEW_PART_SHIP_FLG', 'SHIP_SVC_BU_ID', 'ITM_QTY',
-                                  'ITM_COMDTY_ID', 'PART_ORD_SEQ_NBR', 'PART_ORD_LN_NBR', 'NEW_PART_FLG', 'SUBSTT_FLG',
-                                  'TMZN_LOC_ID', 'DW_LD_GRP_VAL', 'ITM_COST', 'ISS_CODE', 'COST', 'TWO_WEEK_TR',
-                                  'SPMD_FBSC', 'OneCost']
+        self.numerical_columns = ['COUNTRY', 'SOURCE', 'DISPATCH_TYPE', 'CALL_TYPE', 'REASON_CODE', 'STATUS',
+                                  'PARTS_STATUS', 'LOCAL_CHANNEL', 'SVC_TYPE', 'SVC_LEVEL', 'SVC_OPTIONS',
+                                  'PROD_TYPE_DESC', 'PROD_GRP_DESC', 'LOB_CODE', 'LOB_DESC', 'PROD_LN_DESC',
+                                  'SYS_CLASS', 'ZIP_SHIPPING', 'KYHD', 'ACCIDENTAL_DAMAGE', 'COMPLETE_CARE',
+                                  'TAX_XMPTN_FLG', 'CURRENCY_CODE', 'KYC', 'ITM_NBR', 'DSPCH_ORD_STAT_CD',
+                                  'MOST_XPSV_PART_FLG', 'DSTRB_HUB_NM', 'PART_TYPE_CD', 'ITEM_NUM', 'ITEM_DESC',
+                                  'AGE_CLASS', 'COMMODITY', 'COMMODITY_DESC', 'ASP_FAMILY', 'RFC', 'REPAIR',
+                                  'RSL_ACTIVE_FLAG', 'BOM_Parent_Part', 'AMER_PLNR_NAME', 'EMEA_PLNR_NAME',
+                                  'APJ_PLNR_NAME', 'FULFIL_CENTER', 'SPMD_ACTIVE_FLAG', 'SPMD_COMMODITY']
 
         # Private variable of the encoded version of the data
         self._encoded_data = None
 
-        # If there is a path for the encoder and for the data the class will sanitize and encode the data for the model
-        if encoder_path is not None and data_path is not None:
-            self.sanitize()
-            self.encode()
+        # Sanitize the data for prediction and encoding
+        if data_path is not None:
+            self._sanitize()
 
     def loadModel(self, model):
         """
         model - already loaded from tf.keras.model.load('model path')
         """
-        self.model = model
+        self._model = model
 
     def loadEncoder(self, encoder):
         """
         encoder - the encoder loaded from joblib.load
         """
-        self.encoder = encoder
+        self._encoder = encoder
 
     def loadData(self, data):
         """
@@ -101,8 +96,10 @@ class SlowDispatchPrediction:
         """
         if isinstance(data, pd.DataFrame):
             self.data = data
+        else:
+            print("Data needs to be instance of DataFrame")
 
-    def sanitize(self):
+    def _sanitize(self):
         """
         Creates the ideal data frame by selecting the needed columns for a prediction.
         Performs data sanitization by replacing missing values for categorical with "NULL" and "0" for numerical.
@@ -128,7 +125,7 @@ class SlowDispatchPrediction:
             # makes the data property be composed only of the columns declared in the constructor
             self.data = self.data[columns]
 
-    def encode(self):
+    def _encode(self):
         """
         Encodes the data property of the object to numerical for making a prediction.
         WARNING! If the data that is predicted on contains any extra data that the model has not seen,
@@ -136,27 +133,29 @@ class SlowDispatchPrediction:
         """
         try:
             if self.data is not None:
-                self._encoded_data = self.encoder.transform(self.data)
-        except Exception:
+                self._encoded_data = self._encoder.transform(self.data)
+        except Exception as e:
             print("There was an error while encoding the data")
-            print(Exception)
+            print(e)
 
-    def predictBatch(self, return_dict=False):
-        # TODO: Implement saving the predictions to file
+    def predictSlowDispatchBatch(self, return_dict=False):
         """
-        Use this function if there were no errors during the encoding of the data
+        This function predicts in batches, this can raise errors so best used in a try/except block
 
         :param return_dict: if True returns a dictionary with the predicted classes and the probabilities
                 for the classes
         :return: array of predicted classes
         """
         if self.data is not None:
+            self._sanitize()
+            self._encode()
+
             if not return_dict:
-                return (self.model.predict(self._encoded_data) > 0.5).astype("int32")
+                return (self._model.predict(self._encoded_data) > 0.5).astype("int32")
             else:
                 return {
-                    "classes": (self.model.predict(self._encoded_data) > 0.5).astype("int32"),
-                    "probability": self.model.predict(self._encoded_data)
+                    "classes": (self._model.predict(self._encoded_data) > 0.5).astype("int32"),
+                    "probability": self._model.predict(self._encoded_data)
                 }
         else:
             print("Data has not been passed to the object, please use loadData(data_path)")
@@ -172,16 +171,42 @@ class SlowDispatchPrediction:
         :return: dictionary of predicted classes, probabilities and data samples that had errors
         """
 
-        # TODO: Implement predicting sequentially on the data and return predictions,
-        #  for error sample save them separately
-        # TODO: Implement saving the results to file and save in the same file predictions and error samples
+        results = {
+            "rows": [],  # Contains the indexes of the rows that had errors
+            "classes": [],
+            "probability": []
+        }
 
-        # Read file
-        #   Loop trough data samples then encode and predict each row
-        #       Handle possible errors and save the error rows for later
-        #   Match prediction with the row in the data and save the row and the prediction in a dict
-        #   Save predictions and errors in a new column on the data passed in a csv file
-        pass
+        # Sanitize data
+        if self.data is not None:
+            self._sanitize()
+
+        slice_index = 0
+
+        for index, row in self.data.iterrows():
+            try:
+                encoded = self._encoded_data = self._encoder.transform(self.data[slice_index:slice_index+1])
+                results["classes"].append((self._model.predict(encoded) > 0.5).astype("int32")[0][0])
+                results["probability"].append((self._model.predict(encoded))[0][0])
+                results["rows"].append(index)
+            except Exception as e:
+                results["rows"].append(index)
+                results["classes"].append("ERROR")
+                results["probability"].append("ERROR")
+
+            # Increment slice index in order to move to the next row of data
+            slice_index += 1
+
+        if output_csv is False:
+            return results
+        else:
+            self.data["SLOW_DISPATCH"] = results["classes"]
+            self.data["PROBABILITY"] = results["probability"]
+            self.data.to_csv("./predictions.csv", index=False)
+
+    def printDataInfo(self):
+        print(f"Shape {self.data.shape}")
+        print(self.data.head())
 
     def __str__(self):
-        return str(self.model.summary())
+        return str(self._model.summary())
--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from dellai import SlowDispatchPrediction
 import pandas as pd
 
-csv = pd.DataFrame(pd.read_csv("./dell_data.csv"))
-
-data = csv.loc[1:2]
+csv = pd.read_csv("./dell_data.csv")
 
 network = SlowDispatchPrediction(model_path="model_recall_0.7809606986899563",
-                                 encoder_path="missing_data_encoder.joblib")
+                                 encoder_path="missing_data_encoder.joblib",
+                                 data_path="./dell_data.csv")
+# network.encode()
 
-network.loadData(data)
-network.sanitize()
-network.encode()
-
-predictions = network.predictBatch()
-
-print(predictions)
+# predictions = network.predictBatch()
+# print(predictions)
 # print(network)
+# network.printDataInfo()
+
+network.predictSequentially(output_csv=True)
+
+# print(csv[0:1])
+# print(csv[1:2])
+# print(csv[2:3])
